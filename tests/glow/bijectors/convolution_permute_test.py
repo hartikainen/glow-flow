f0a56709327f81d30a098674143575937d8ef567
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-07-10 15:35:56 -0700

    Add tests for ConvolutionPermute

---
+++ tests/glow/bijectors/convolution_permute_test.py
@@ -0,0 +1,68 @@
+import numpy as np
+import tensorflow as tf
+import snapshottest
+
+from glow.bijectors import ConvolutionPermute
+
+
+tf.enable_eager_execution()
+tf.set_random_seed(1)
+
+
+class TestConvolutionPermute(tf.test.TestCase, snapshottest.TestCase):
+
+    def setUp(self):
+        self.batch_size = batch_size = 1
+        self.event_dims = event_dims = (4,4,3)
+        self.bijector = ConvolutionPermute(
+            event_ndims=len(event_dims),
+            event_dims=event_dims,
+            validate_args=False)
+
+    def testForward(self):
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+
+        z = self.bijector.forward(x)
+
+        with self.test_session():
+            self.assertMatchSnapshot(z.numpy().tolist())
+
+    def testInverse(self):
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+
+        z = self.bijector.inverse(x)
+
+        with self.test_session():
+            self.assertMatchSnapshot(z.numpy().tolist())
+
+    def testForwardLogDetJacobian(self):
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+
+        log_det_jacobian = self.bijector.forward_log_det_jacobian(x)
+
+        with self.test_session():
+            self.assertMatchSnapshot(log_det_jacobian.numpy().tolist())
+
+    def testInverseLogDetJacobian(self):
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+
+        inverse_log_det_jacobian = self.bijector.inverse_log_det_jacobian(x)
+
+        with self.test_session():
+            self.assertMatchSnapshot(inverse_log_det_jacobian.numpy().tolist())
+
+    def testBijective(self):
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+        x_ = self.bijector.inverse(self.bijector.forward(x))
+
+        with self.test_session():
+            self.assertAllEqual(x, x_.numpy())
+
+
+if __name__ == '__main__':
+  tf.test.main()
