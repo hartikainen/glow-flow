c996da316dfcc3c4e83258a814f7ca6367058d9c
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-07-22 16:43:42 -0700

    Implement model_fn in train.py

--- scripts/train.py
+++ scripts/train.py
@@ -92,6 +92,31 @@ def parse_args():
     return args
 
 
+def bits_per_dim(negative_log_likelihood, image_shape):
+    image_size = tf.reduce_prod(image_shape)
+    return ((negative_log_likelihood + tf.log(256.0) * image_size)
+            / (image_size * tf.log(2.0)))
+
+
+def pack_images(images, rows, cols):
+    """Helper utility to make a field of images."""
+    shape = tf.shape(images)
+    width, height, depth = shape[-3], shape[-2], shape[-1]
+    images = tf.reshape(images, (-1, width, height, depth))
+    batch = tf.shape(images)[0]
+    rows = tf.minimum(rows, batch)
+    cols = tf.minimum(batch // rows, cols)
+    images = images[:rows * cols]
+    images = tf.reshape(images, (rows, cols, width, height, depth))
+    images = tf.transpose(images, [0, 2, 1, 3, 4])
+    images = tf.reshape(images, [1, rows * width, cols * height, depth])
+    return images
+
+
+def image_tile_summary(name, tensor, rows=8, cols=8):
+    tf.summary.image(name, pack_images(tensor, rows, cols), max_outputs=1)
+
+
 def model_fn(features, labels, mode, params, config):
     """Build the glow flow model function for use in an estimator.
 
@@ -104,7 +129,92 @@ def model_fn(features, labels, mode, params, config):
         Returns:
         EstimatorSpec: A tf.estimator.EstimatorSpec instance.
     """
-    # TODO: Implement this
+    base_distribution = tfd.MultivariateNormalDiag(
+        loc=tf.zeros(features.shape[-3:]),
+        scale_diag=tf.ones(features.shape[-3:]))
+
+    glow_flow = GlowFlow(
+        num_levels=params['num_levels'],
+        level_depth=params['level_depth'])
+
+    transformed_glow_flow = tfd.TransformedDistribution(
+        distribution=base_distribution,
+        bijector=glow_flow,
+        name="transformed_glow_flow")
+
+    image_tile_summary("input", tf.to_float(features), rows=1, cols=16)
+
+    z = glow_flow.inverse(features)
+    prior_log_probs = base_distribution.log_prob(z)
+    prior_log_likelihood = -tf.reduce_mean(prior_log_probs)
+    log_det_jacobians = glow_flow.inverse_log_det_jacobians(features)
+    log_probs = log_det_jacobians + log_det_jacobians
+
+    # Sanity check, remove when tested
+    assert tf.equal(log_probs, transformed_glow_flow.log_prob(features))
+
+    negative_log_likelihood = -tf.reduce_mean(log_probs)
+    bpd = bits_per_dim(negative_log_likelihood, features.shape[-3:])
+
+    loss = negative_log_likelihood
+
+    tf.summary.scalar(
+        "negative_log_likelihood",
+        tf.reshape(negative_log_likelihood, []))
+    tf.summary.scalar("bit_per_dim", tf.reshape(bpd, []))
+
+    #  TODO: prior likelihood and log det jacobians?
+    # tf.summary.scalar("prior_ll", tf.reshape(tf.reduce_mean(prior_ll), []))
+
+    z_l2 = tf.norm(z, axis=1)
+    z_l2_mean, z_l2_var = tf.nn.moments(z_l2)
+    log_det_jacobians_mean, log_det_jacobians_var = tf.nn.moments(
+        log_det_jacobians)
+    prior_log_likelihood_mean, prior_log_likelihood_var = tf.nn.moments(
+        prior_log_likelihood)
+
+    tf.summary.scalar("log_det_jacobians_mean",
+                      tf.reshape(log_det_jacobians_mean, []))
+    tf.summary.scalar("log_det_jacobians_var",
+                      tf.reshape(log_det_jacobians_var, []))
+
+    tf.summary.scalar("prior_log_likelihood_mean",
+                      tf.reshape(prior_log_likelihood_mean, []))
+    tf.summary.scalar("prior_log_likelihood_var",
+                      tf.reshape(prior_log_likelihood_var, []))
+
+    tf.summary.scalar("l2_z_mean", tf.reshape(z_l2_mean, []))
+    tf.summary.scalar("z_l2_var", tf.reshape(z_l2_var, []))
+
+    # Generate samples for visualization.
+    random_image = transformed_glow_flow.sample(16)
+    image_tile_summary(
+        "random/sample", tf.to_float(random_image), rows=4, cols=4)
+
+    global_step = tf.train.get_or_create_global_step()
+    learning_rate = tf.train.cosine_decay(
+        params['learning_rate'], global_step, params['max_steps'])
+    tf.summary.scalar("learning_rate", learning_rate)
+
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    gradients, variables = zip(*optimizer.compute_gradients(loss))
+    capped_gradients, gradient_norm = tf.clip_by_global_norm(
+        gradients, clip_norm=params['clip_gradient'])
+    capped_gradients_and_variables = zip(capped_gradients, variables)
+    train_op = optimizer.apply_gradients(
+        capped_gradients_and_variables, global_step=global_step)
+
+    gradient_norm = tf.check_numerics(
+        gradient_norm, "Gradient norm contains NaNs or Infs.")
+    tf.summary.scalar("gradient_norm", tf.reshape(gradient_norm, []))
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops={
+            "log_probs": tf.metrics.mean(log_probs),
+        })
 
 
 def train_model(args):
