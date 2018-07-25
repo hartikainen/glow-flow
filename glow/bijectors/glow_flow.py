8719e2311ee64034402f58bdbbeea75b766a675b
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-07-24 19:52:20 -0700

    Update GlowFlow build method and tests

--- glow/bijectors/glow_flow.py
+++ glow/bijectors/glow_flow.py
@@ -176,14 +176,8 @@ class GlowFlow(tfb.Bijector):
         self._input_shape = input_shape
         self._image_shape = input_shape[1:]
 
-        levels = [
-            GlowStep(
-                input_shape=input_shape[1:],
-                depth=self._level_depth,
-                name="glow_step_0"),
-        ]
         levels = []
-        for i in range(1, self._num_levels):
+        for i in range(self._num_levels):
             # Every level split the input in half (on the channel-axis),
             # and applies the next level only to the half of the split.
             # The other half flows directly into the output z. NOTE:
@@ -192,16 +186,23 @@ class GlowFlow(tfb.Bijector):
             # paper however.
             # See: https://github.com/openai/glow/blob/master/model.py#L485
             levels.append(
-                Parallel(
-                    bijectors=[
-                        GlowStep(
-                            input_shape=input_shape[1:],
-                            depth=self._level_depth,
-                            name="glow_step_{}".format(i)),
-                        tfb.Identity()
-                    ],
-                    split_axis=-1,
-                    split_proportions=[1, 2**(i)-1]))
+                tfb.Chain([
+                    tfb.Invert(Squeeze(factor=2**(i+1))),
+                    Parallel(
+                        bijectors=[
+                            GlowStep(
+                                # Infer at the time of first forward
+                                input_shape=None,
+                                depth=self._level_depth,
+                                name="glow_step_{}".format(i)),
+                            tfb.Identity(),
+                        ],
+                        split_axis=-1,
+                        split_proportions=[1, 2**(i)-1]
+                    ),
+                    Squeeze(factor=2**(i+1))
+                ])
+            )
 
         # Note: tfb.Chain applies the list of bijectors in the _reverse_ order
         # of what they are inputted.
--- tests/glow/bijectors/glow_flow_test.py
+++ tests/glow/bijectors/glow_flow_test.py
@@ -14,25 +14,52 @@ class TestGlowFlow(tf.test.TestCase, snapshottest.TestCase):
     def setUp(self):
         self.batch_size = 1
         # event_dims = image_size
-        self.event_dims = (32, 32, 3)
+        self.event_dims = (8, 8, 3)
+
 
     def testBijective(self):
-        flow = GlowFlow(
-            num_levels=2,
-            level_depth=2,
-            validate_args=False)
+        flow = GlowFlow(num_levels=3, level_depth=3, validate_args=False)
         x = tf.random_uniform(
             (self.batch_size, ) + self.event_dims, dtype=tf.float32)
-        # Squeeze to get even last dimension size
-        x = Squeeze(factor=2).forward(x)
         z = flow.inverse(x)
         x_ = flow.forward(tf.identity(z))
 
         assert z.shape == x.shape
 
-
         with self.test_session():
             self.assertAllEqual(x, x_.numpy())
+
+    def testForward(self):
+        flow = GlowFlow(num_levels=3, level_depth=3, validate_args=False)
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+        z = flow.forward(x)
+
+        # with self.test_session():
+        #     self.assertMatchSnapshot(x.numpy().tolist())
+
+    def testInverse(self):
+        flow = GlowFlow(num_levels=3, level_depth=3, validate_args=False)
+        x = tf.random_uniform(
+            (self.batch_size, ) + self.event_dims, dtype=tf.float32)
+        z = flow.inverse(x)
+
+        # with self.test_session():
+        #     self.assertMatchSnapshot(z.numpy().tolist())
+
+    def testParallelPassthrough(self):
+        num_levels = 3
+        flow = GlowFlow(
+            num_levels=num_levels, level_depth=2, validate_args=False)
+        event_dims = (64, 64, 8)
+        x = tf.random_uniform(
+            (self.batch_size, ) + event_dims, dtype=tf.float32)
+        z = flow.forward(x)
+
+        for level in range(num_levels):
+            # TODO: Test somehow that all the passthrough values match
+            # expectation
+            pass
 
 
 if __name__ == '__main__':
