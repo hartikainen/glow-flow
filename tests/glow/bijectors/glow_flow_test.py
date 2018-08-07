7a7fcaff1400578384ad58553790509d93950ded
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-08-07 16:04:39 -0700

    Update GlowFlow tests

--- tests/glow/bijectors/glow_flow_test.py
+++ tests/glow/bijectors/glow_flow_test.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 import snapshottest
 
 from glow.bijectors import GlowFlow, GlowStep
 from glow.bijectors.squeeze import Squeeze
 
+tfb = tfp.bijectors
 
 tf.enable_eager_execution()
 tf.set_random_seed(1)
@@ -15,6 +17,7 @@ class TestGlowFlow(tf.test.TestCase, snapshottest.TestCase):
         self.batch_size = 1
         # event_dims = image_size
         self.event_dims = (8, 8, 3)
+        tf.reset_default_graph()
 
     def testBijective(self):
         flow = GlowFlow(level=3, level_depth=3, validate_args=False)
@@ -64,16 +67,35 @@ class TestGlowFlow(tf.test.TestCase, snapshottest.TestCase):
         event_dims = (64, 64, 8)
         x = tf.random_uniform(
             (self.batch_size, ) + event_dims, dtype=tf.float32)
+
         z = flow.forward(x)
 
-        for level in range(num_levels):
-            # TODO: Test somehow that all the passthrough values match
-            # expectation
-            pass
+        z2 = tfb.Chain(list(reversed(
+            flow.flow_steps[:2] + flow.flow_steps[-1:]
+        ))).forward(x)
+
+        self.assertNotEqual(z, z2)
+        self.assertTrue(tf.reduce_all(
+            tf.equal(z[..., :event_dims[-1]/2], z2[..., :event_dims[-1]/2])
+        ))
 
     def testVerifyTrainableVariables(self):
+        num_levels = 3
+        flow = GlowFlow(
+            level=num_levels, level_depth=2, validate_args=False)
+        event_dims = (64, 64, 8)
+        x = tf.random_uniform(
+            (self.batch_size, ) + event_dims, dtype=tf.float32)
+
+        flow.forward(x)
+
+        trainable_variables = tf.trainable_variables()
+
         raise NotImplementedError(
             "Should test that the trainable variables match expectation")
+
+        with self.test_session():
+            self.assertMatchSnapshot(trainable_variables)
 
 
 class TestGlowStep(tf.test.TestCase, snapshottest.TestCase):
